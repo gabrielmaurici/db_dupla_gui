c94849ed462e468530a56900d3f0569dd96a6847
Eduardo Weber <edu.weber07@gmail.com>
2021-08-11 13:41:32 -0300

    Iniciando do zero dnv

---
+++ BibliotecaProdCat/ProdCad/Context/BaseContext.cs
@@ -0,0 +1,25 @@
+﻿using ProdCad.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProdCad.Context
+{
+    class BaseContext : DbContext
+    {
+        public DbSet<Produtos> Produtos { get; set; }
+        public DbSet<Categorias> Categorias{ get; set; }
+        public BaseContext() : base ("ProdCatDB")
+        {
+
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
